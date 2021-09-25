86da73d907c293b5025f047a3ec6300ab8a194a9
syedishad <82176036+syedishad@users.noreply.github.com>
2021-09-25 17:36:03 +0530

    Add files via upload

---
+++ hussain.java
@@ -0,0 +1,9 @@
+public class hussain{
+public static void main(String args[]){
+System.out.println("learing java coding and excuting in jdk application");
+System.out.println("its really tough language.full of headace");
+System.out.println("and not much to say about it");
+System.out.println("but after margine of zero error ,having full to do it!!!");
+System.out.println("at the end ,,,,,thankyou!!!!!!");
+}
+}
