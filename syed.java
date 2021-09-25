dc23a616996b190b4b115e6b4a50b637b3d2010f
syedishad <82176036+syedishad@users.noreply.github.com>
2021-09-25 17:38:01 +0530

    Add files via upload

---
+++ syed.java
@@ -0,0 +1,6 @@
+public class syed{
+public static void main(String args[]){
+System.out.println("my name is syed ishad husssain,studying in b.tech 2nd year ..");
+System.out.println("i like to read novels and love to play pubg");
+}
+}
