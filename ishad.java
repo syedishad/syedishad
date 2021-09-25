96e5dc1d0522114eb39a3634cbcc4a2c4be44f84
syedishad <82176036+syedishad@users.noreply.github.com>
2021-09-25 17:37:09 +0530

    Add files via upload

---
+++ ishad.java
@@ -0,0 +1,8 @@
+public class ishad{
+public static void main(String args[])
+{
+System.out.println("i like to reads books and love to play pubg");
+
+}
+}
+                                                       
