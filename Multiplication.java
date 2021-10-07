89cf0e057c5f56784fa09797a3e1632689f26959
syedishad <82176036+syedishad@users.noreply.github.com>
2021-10-07 21:01:20 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Multiplication
+{
+      public static void main(String args[])
+      {
+ 
+           int Input1,Input2,mul;
+           Scanner sc=new Scanner(System.in);
+
+           System.out.println("Enter The Input 1:");
+           Input1=sc.nextInt();
+           System.out.println("Enter The Input 2:");
+           Input2=sc.nextInt();
+           mul=Input1*Input2;
+           System.out.println("The Output Is:\n"+mul);
+      }
+
+}
